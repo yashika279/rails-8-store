7d2756a687c740922cb0974dfab52166be8f35e2
Yashika <yashikavijay2799@gmail.com>
2026-05-05 18:04:11 +0530

    Edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,19 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def product_params
       params.expect(product: [ :name ])
