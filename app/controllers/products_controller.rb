ade06c72831de8377fcb48d8ead2f6dfa7ddfb84
Yashika <yashikavijay2799@gmail.com>
2026-05-05 18:07:56 +0530

    Before Action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update ]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def new
@@ -21,11 +22,9 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to @product
     else
@@ -34,6 +33,11 @@ class ProductsController < ApplicationController
   end
 
   private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
     def product_params
       params.expect(product: [ :name ])
     end
